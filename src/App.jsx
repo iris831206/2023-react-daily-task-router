8205298fdd043becf62e460d5034713411695078
iris831206 <140391994+iris831206@users.noreply.github.com>
2023-08-31 21:07:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, NavLink } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, Outlet, useParams } from 'react-router-dom';
 
 const Todo = () => {
   return <p>這是 Todo 頁面</p>;
@@ -10,6 +10,28 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
+
+const Home = () => {
+  return <p>這是首頁</p>;
+};
+
+const Post = () => {
+  return (
+    <>
+      <h2>POST</h2>
+      <Outlet />
+    </>
+  )
+};
+
+const PostId = () => {
+  
+  let params = useParams();
+
+  return (
+    <p>Your Post ID: {params.postId}</p>
+  )
+}
 
 function App() {
   return (
@@ -28,9 +50,33 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/12345">
+            <p>Post 詳細</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/todo' element={<Todo />} />
+          {/* 巢狀路由 */}
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
       </HashRouter>
     </div>
   );
